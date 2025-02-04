faace2b739711d1da96cab821dba114d82e875ed
hessnt30 <hessnt30@vt.edu>
2025-02-04 17:05:24 -0500

    initialize next app

---
+++ next-app/src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "CrisisBrief",
+  description: "Crisis Event Template Generation",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <main className="flex flex-1 min-h-screen bg-background w-full">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ next-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-1 items-center justify-center min-h-screen bg-background">
+      <h1>CrisisBrief</h1>
+    </div>
+  );
+}
