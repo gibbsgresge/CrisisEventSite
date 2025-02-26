4aa1e84b18bcc79cc0325a42188c4fec840a2072
hessnt30 <hessnt30@vt.edu>
2025-02-26 00:02:46 -0500

    got login and register forms styled

---
+++ next-app/src/components/header.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+export default function Header() {
+  return (
+    <header className="border-b bg-background flex flex-1 justify-center">
+      <div className="container flex h-16 items-center justify-between">
+        <div className="flex items-center gap-6">
+          <Link href={"/"}>
+            <h1>CrisisBrief</h1>
+          </Link>
+        </div>
+
+        <div className="flex items-center gap-6">
+          <Link href={"/login"}>
+            <Button>Login</Button>
+          </Link>
+          <Link href={"/register"}>
+            <Button variant={"outline"}>Register</Button>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
